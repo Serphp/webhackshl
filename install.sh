f40a2f3b95463b49b01570754f98ad17d948633d
Eduard Toloza <tolosaeduard@gmail.com>
2018-03-19 11:17:13 -0500

    Modificado print de mensajes y añadido desinstalador.

--- install.sh
+++ install.sh
@@ -4,22 +4,22 @@ ROOT_UID=0
 
 function webhackinstall() {
 	if [ -f /etc/debian_version ] ; then
-		echo -e 'Procediendo con la instalación en tu sistema Debian o basado en Debian.'
+		echo -e "Procediendo con la instalación en tu sistema Debian o basado en Debian."
 		mkdir /opt/webhackshl/
 		cp -R * /opt/webhackshl/
-		echo -e '\n#!/usr/bin/bash\npython /opt/webhackshl/webhackshl.py "$@"' | sudo tee -a /usr/bin/webhackshl > /dev/null
+		echo -e "\n#!/usr/bin/bash\npython /opt/webhackshl/webhackshl.py" | sudo tee -a /usr/bin/webhackshl > /dev/null
 		chmod +x /usr/bin/webhackshl
-		echo -e 'Instalación finalizada, ahora puedes ejecutar el comando "webhackshl" directamente desde tu terminal.\n'
+		echo -e "Instalación finalizada, ahora puedes ejecutar el comando "webhackshl" directamente desde tu terminal.\n"
 
 	elif [ -f /etc/arch-release ] ; then
-		echo -e 'Procediendo con la instalación en tu sistema ArchLinux o basado en ArchLinux.\n'
+		echo -e "Procediendo con la instalación en tu sistema ArchLinux o basado en ArchLinux.\n"
 		mkdir /opt/webhackshl/
 		cp -R * /opt/webhackshl/
-		echo -e '\n#!/usr/bin/bash\npython2 /opt/webhackshl/webhackshl.py "$@"' | sudo tee -a /usr/bin/webhackshl > /dev/null
+		echo -e "\n#!/usr/bin/bash\npython2 /opt/webhackshl/webhackshl.py" | sudo tee -a /usr/bin/webhackshl > /dev/null
 		chmod +x /usr/bin/webhackshl
-		echo -e 'Instalación finalizada, ahora puedes ejecutar el comando "webhackshl" directamente desde tu terminal.\n'
+		echo -e "Instalación finalizada, ahora puedes ejecutar el comando "webhackshl" directamente desde tu terminal.\n"
 	else
-		echo -e '\nSistema no identificado, saliendo.'
+		echo -e "\nSistema no identificado, saliendo."
 		exit
 	fi
 }
